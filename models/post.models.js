004b84c7e264d475608ed700bd63c365f749f840
agrawal542 <gautamagrawal542@gmail.com>
2023-12-06 16:42:51 +0530

    Initial commit

---
+++ models/post.models.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+  title :{
+    type : String,
+    required:true,
+    unique:true
+  },
+  content :{
+    type : String,
+    required:true,
+  }
+})
+
+export const Post = mongoose.model("Post",postSchema)
